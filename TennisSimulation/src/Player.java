f1d745c1169501ec25c1442af8929cc623767db3
Unknown <balogh.gergely83@gmail.com>
2018-05-15 07:48:52 +0200

    Tenisz

--- TennisSimulation/src/Player.java
+++ TennisSimulation/src/Player.java
@@ -1,65 +1,83 @@
+import java.util.Map;
 import java.util.Random;
 import java.util.stream.IntStream;
 
-public class PlayerDraw {
-	private static String[] nameArray = { "Roger Federer", "Rafael Nadal", "Novak Đoković", "Pete Sampras",
-			"Andre Agassi", "Björn Borg", "Boris Becker", "Andy Roddick" };
+public class Player {
+	private static Map<Integer, String> name = Map.of(0, "Roger Federer", 1, "Rafael Nadal", 2, "Novak Đoković", 3,
+			"Pete Sampras", 4, "Andre Agassi", 5, "Björn Borg", 6, "Boris Becker", 7, "Andy Roddick");
+	public static Map<String, Integer> point = Map.of("Roger Federer", 0, "Rafael Nadal", 0, "Novak Đoković", 0,
+			"Pete Sampras", 0, "Andre Agassi", 0, "Björn Borg", 0, "Boris Becker", 0, "Andy Roddick", 0);
+	public static String getName(int index) {
+		return name.get(index);
+	}
+
+	static int index = 0;
+	static Random random = new Random();
+	static int[] indexOfArray = new int[8];
+	static int[] indexOfLoserArray = new int[4];
 	static String[] firstPair = new String[2];
 	static String[] secondPair = new String[2];
 	static String[] thirdPair = new String[2];
 	static String[] fourthPair = new String[2];
-	static int[] indexOfArray = new int[8];
-	static int index = 0;
+	static String[] winners = new String[4];
+	static String[] winnersWinners = new String[2];
+	static String[] winnersLosers = new String[2];
+	static String[] losers = new String[4];
 
-	public static void main(String[] args) {
-		PlayerDraw.randomPlayer();
-		PlayerDraw.fillPairArrays();
-		PlayerDraw.print();
-	}
-	static Random random = new Random();
 	
-	public static void randomPlayer() {
+	
+	public static void randomPlayer(int[] sequence, String[] playerArray) {
+		String[] arrayName = playerArray;
+		int[] indexArray = sequence;
 		boolean contains;
-		for (int i = 0; i < 7; i++) {
+		for (int i = 0; i < indexArray.length - 1; i++) {
 			do {
-				index = random.nextInt(nameArray.length);
-				contains = IntStream.of(indexOfArray).anyMatch(x -> x == index);
+				index = random.nextInt(arrayName.length);
+				contains = IntStream.of(indexArray).anyMatch(x -> x == index);
 			} while (contains);
-			indexOfArray[i] = index;
+			indexArray[i] = index;
 		}
 	}
-	
-	public static void fillPairArrays() {
-		for (int i = 0; i < indexOfArray.length; i++) {
+
+	/**
+	 * Játékosok párosítása
+	 */
+	public static void fillPairArrays(String[] playerArray, int length) {
+		String[] nameArray = playerArray;
+		int index = length;
+		for (int i = 0; i < index; i++) {
 			switch (i) {
 			case 0:
-				firstPair[0] = nameArray[indexOfArray[i]];
+				firstPair[0] = nameArray[i];
 				break;
 			case 1:
-				firstPair[1] = nameArray[indexOfArray[i]];
+				firstPair[1] = nameArray[i];
 				break;
 			case 2:
-				secondPair[0] = nameArray[indexOfArray[i]];
+				secondPair[0] = nameArray[i];
 				break;
 			case 3:
-				secondPair[1] = nameArray[indexOfArray[i]];
+				secondPair[1] = nameArray[i];
 				break;
 			case 4:
-				thirdPair[0] = nameArray[indexOfArray[i]];
+				thirdPair[0] = nameArray[i];
 				break;
 			case 5:
-				thirdPair[1] = nameArray[indexOfArray[i]];
+				thirdPair[1] = nameArray[i];
 				break;
 			case 6:
-				fourthPair[0] = nameArray[indexOfArray[i]];
+				fourthPair[0] = nameArray[i];
 				break;
 			case 7:
-				fourthPair[1] = nameArray[indexOfArray[i]];
+				fourthPair[1] = nameArray[i];
 				break;
 			}
 		}
 	}
-	
+
+	/**
+	 * A játékosok párba állítása utáni lista
+	 */
 	public static void print() {
 		System.out.println();
 		System.out.println(firstPair[0] + " - " + firstPair[1]);
@@ -67,4 +85,19 @@ public class PlayerDraw {
 		System.out.println(thirdPair[0] + " - " + thirdPair[1]);
 		System.out.println(fourthPair[0] + " - " + fourthPair[1]);
 	}
+
+	public static void printWinnerAndLoser(int index) {
+		for (int i = 0; i < winners.length; i++) {
+			System.out.println(index + " meccsén nyert: " + winners[i]);
+			System.out.println(index + " meccsén vesztett: " + losers[i]);
+		}
+	}
+	
+	public static void emptyPlayerlist() {
+		firstPair = new String[2];
+		secondPair = new String[2];
+		thirdPair = new String[2];
+		fourthPair = new String[2];
+	}
+	
 }
