ee22add667c1d99134cd1b7187edec66606a6474
Balogh Gergely <gergely.balogh@icellmobilsoft.hu>
2018-06-18 17:28:44 +0200

    SHA512 crypter

--- Crypter/src/Main.java
+++ Crypter/src/Main.java
@@ -4,9 +4,10 @@ public class Main {
 
 	public static void main(String[] args) throws NoSuchAlgorithmException {
 
-		String str = "Teszt1234";
-		byte[] hash = Sha256Crypter.Sha256(str);
-		System.out.println(hash);
+		String str = "OlMJyBw14BOF0m1esEEV";
+		byte[] hash256 = Sha256Crypter.Sha256(str);
+		String hash512 = SHA512Crypter.get_SHA_512_SecurePassword(str);
+		System.out.println("" + hash256 + "\n" + hash512);
 		
 		// 369D3CE0EE61B5D3E4192160BC57D51D904C8D5E8748D834142DCFCA044BDC28
 		
