0ad99d45d05101d40ec57be18463b1bcea8226d6
Balogh Gergely <gergely.balogh@icellmobilsoft.hu>
2018-06-20 17:14:48 +0200

    Armstrong számok javítás
    A validátor hibás az 1 karakter hosszú számoknál!

--- ArmstrongNumber/src/ArmstrongNumber.java
+++ ArmstrongNumber/src/ArmstrongNumber.java
@@ -2,38 +2,36 @@ import java.util.Scanner;
 
 public class ArmstrongNumber {
 	static Scanner sc;
-	static long nSum;
-	static long nDigits;
 	static long nLastDigitPow;
 	static int n;
-	static int length;
-	static int piece = 1;
-	static int nOriginal;
-	static long i;
 
 	public static void main(String[] args) {
-
+		long i;
+		int length;
+		int piece = 1;
+		long startTime = System.currentTimeMillis();
+		long nDigits;
+		long nSum = 0;
+		int nOriginal = n;
+		long j = 0;
 		getN();
 		
-		long startTime = System.currentTimeMillis();
-		nOriginal = n;
-		long j = 0;
 		do {
 			nDigits = j;
 			if (j == 0) {
-				System.out.println(piece + " - Armstong szám: " + (long) nSum);
+				System.out.println(piece + " - Armstong szám: " + nSum);
 				piece++;
 			} else {
 				length = (int) (Math.log10(j) + 1);
 				for (i = 0; i < length; i++) {
-					long nLastDigit = (long) nDigits % 10;
+					long nLastDigit = nDigits % 10;
 					nDigits = nDigits / 10;
 					pow(nLastDigit, length);
 					nSum = nSum + nLastDigitPow;
 					nLastDigitPow = 0;
 				}
 				if (nSum == j) {
-					System.out.println(piece + " - Armstong szám: " + (long) nSum);
+					System.out.println(piece + " - Armstong szám: " + nSum);
 					System.out.println("A futási idő: " + (System.currentTimeMillis() - startTime) / 1000 + " másodperc.");
 					piece++;
 				}
@@ -47,11 +45,16 @@ public class ArmstrongNumber {
 		sc = new Scanner(System.in);
 		System.out.println("Armstrong szám vizsgálat.\nKérem adjon meg egy 1 és 50 közötti számot.");
 		do {
-			System.out.print("N: ");
-			n = sc.nextInt();
-			sc.nextLine();
-			if (n < 1 || n > 50) {
-				System.out.println("A megadott szám érvénytelen. ");
+			try {
+				System.out.print("N: ");
+				n = sc.nextInt();
+				sc.nextLine();
+				if (n < 1 || n > 50) {
+					System.out.println("A megadott szám érvénytelen. ");
+				}
+			} catch (Exception e) {
+				sc.nextLine();
+				System.out.println("A megadott input érvénytelen.");
 			}
 		} while (n > 50 || n < 1);
 		sc.close();
--- ArmstrongNumber/src/ArmstrongNumberValidator.java
+++ ArmstrongNumber/src/ArmstrongNumberValidator.java
@@ -2,44 +2,49 @@ import java.util.Scanner;
 
 public class ArmstrongNumberValidator {
 
-	static long nSum;
 	static long nDigits;
 	static long number;
-	static int nSumLength;
-	static long nSumSum;
 	static long nLastDigitPow;
 
 	public static void main(String[] args) {
+		long number = getInput();
+		checkNumber(number);
+	}
 
-		Scanner sc = new Scanner(System.in);
-		System.out.print("Kérem adjon meg egy vizsgálandó számot: ");
-		long number = sc.nextLong();
-		sc.nextLine();
-		sc.close();
-
+	private static void checkNumber(long number) {
+		long nSum = 0L;
 		nDigits = number;
 		int length = (int) (Math.log10(number) + 1);
-		long nLastDigit = (long) nDigits % 10;
+		long nLastDigit = nDigits % 10;
 		long j = number;
 		nDigits = j;
 		if (j == 0) {
 		} else {
 			length = (int) (Math.log10(j) + 1);
 			for (int i = 0; i < length; i++) {
-				nLastDigit = (long) nDigits % 10;
+				nLastDigit = nDigits % 10;
 				nDigits = nDigits / 10;
 				pow(nLastDigit, length);
 				nSum = nSum + nLastDigitPow;
 				nLastDigitPow = 0;
 			}
 			if (nSum == j) {
-				System.out.println("Armstong szám: " + (long) nSum);
+				System.out.println("Armstong szám: " + nSum);
 			} else {
 				System.out.println("Nem Armstrong szám.");
 			}
 			nSum = 0;
 		}
 		j++;
+	}
+
+	private static long getInput() {
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Kérem adjon meg egy vizsgálandó számot: ");
+		long number = sc.nextLong();
+		sc.nextLine();
+		sc.close();
+		return number;
 	}
 
 	private static long pow(long nLastDigit, int length) {
