82e972bf9ea6a5ac08b551f5a9dd1ad76942d8e8
KicsiG <balogh.gergely83@gmail.com>
2018-08-02 06:26:59 +0200

    Oszthatóság vizsgálata

--- DivisibilityOOP/src/hu/ak_akademia/divisibility/IntegerDivisionCheck.java
+++ DivisibilityOOP/src/hu/ak_akademia/divisibility/IntegerDivisionCheck.java
@@ -3,7 +3,7 @@ package hu.ak_akademia.divisibility;
 public class IntegerDivisionCheck implements DivisionCheck {
 
 	private int accessor;
-	
+
 	public int getAccessor() {
 		return accessor;
 	}
@@ -11,10 +11,15 @@ public class IntegerDivisionCheck implements DivisionCheck {
 	public IntegerDivisionCheck(int accessor) {
 		this.accessor = accessor;
 	}
-	
+
 	@Override
 	public boolean isDivisible(int userEnteredNumber) {
 		return userEnteredNumber % getAccessor() == 0;
+	}
+
+	@Override
+	public String toString() {
+		return Integer.toString(getAccessor());
 	}
 
 }
--- DivisibilityOOP/src/hu/ak_akademia/divisibility/Main.java
+++ DivisibilityOOP/src/hu/ak_akademia/divisibility/Main.java
@@ -2,46 +2,30 @@ package hu.ak_akademia.divisibility;
 
 public class Main {
 
-	/*
-	 * megvizsgálja 1-től 10-ig, hogy melyik számmal osztható!
-	 * 
-	 * A program az oszthatóság vizsgálathoz OOP-s elveknek megfelelően külön
-	 * példányokat használjon!
-	 * 
-	 * Legyen ennek egy konkrét megvalósítása, ami IntegerDivisionCheck néven fut és
-	 * ami konstruktora paramétereként vár egy egész számot, amit el is tárol egy
-	 * példányváltozójában és amivel való oszthatóságot vizsgálni fogja!
-	 * 
-	 * A példányváltozóhoz hozz létre egy accessor (getter) metódust!
-	 * 
-	 * Az isDivisible metódus legyen megvalósítva az IntegerDivisionCheck osztályban
-	 * is és azt ellenőrizze, hogy a paraméterként megadott felhasználói szám
-	 * osztható-e az IntegerDivisionCheck példány létrehozásakor megadott számmal!
-	 * Ha igen, akkor térjen vissza true értékkel, különben false-szal!
-	 * 
-	 * Az IntegerDivisionCheck osztályból hozz létre 10 példányt és tedd be őket egy
-	 * DivisionCheck típusú tömbbe!
-	 * 
-	 * A példányok létrehozásakor add meg nekik 1-től 10-ig a számokat, hogy az
-	 * ezzel a számmal történő oszthatóságot vizsgálják majd!
-	 * 
-	 * Az 1-től 10-ig való számokkal történő oszthatóságot vizsgáld meg úgy a
-	 * felhasználó által megadott számra, hogy a 10 példány mindegyikén meghívod az
-	 * isDivisible metódust!
-	 * 
-	 * Írd ki a standard kimenetre, hogy a felhasználó által megadott szám melyik
-	 * számmal osztható 1-től 10-ig!
-	 */
-
 	public static void main(String[] args) {
-
 		new Main().run();
-
 	}
 
 	public void run() {
+		DivisionCheck[] dc = new DivisionCheck[10];
 		int number = new UserInput().getNumber();
-		System.out.println(new IntegerDivisionCheck(10).isDivisible(number));
+		fillDivisionCheckArray(dc);
+		callDivisionCheck(dc, number);
 	}
 
+	private void callDivisionCheck(DivisionCheck[] dc, int number) {
+		String separator = " ";
+		System.out.println("A(z) " + number + " osztói: ");
+		for (int i = 0; i < 10; i++) {
+			if (dc[i].isDivisible(number)) {
+				System.out.print(dc[i].toString() + separator);
+			}
+		}
+	}
+
+	private void fillDivisionCheckArray(DivisionCheck[] dc) {
+		for (int i = 0; i < dc.length; i++) {
+			dc[i] = new IntegerDivisionCheck(i + 1);
+		}
+	}
 }
