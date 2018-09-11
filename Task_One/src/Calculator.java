542d85259ab3f5bf1ee31078e9a6c9a38cc170d7
KicsiG <balogh.gergely83@gmail.com>
2018-09-11 07:19:31 +0200

    2. szintfelmérő javítás

--- Task_One/src/Calculator.java
+++ Task_One/src/Calculator.java
@@ -2,7 +2,7 @@
 public class Calculator {
 
 	public void calculate(int number) {
-		for (int i = number; i > 0; i--) {
+		for (int i = number-1; i > 0; i--) {
 			if (Math.sqrt(i) == (int) Math.sqrt(i)) {
 				System.out.println((int) i);
 			}
