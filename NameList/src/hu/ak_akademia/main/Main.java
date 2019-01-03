45782b1237d8671462bef7a5db54b544084e6c34
Balogh Gergely <gergely.balogh@icellmobilsoft.hu>
2019-01-03 09:12:40 +0100

    Névsor folytatás

--- NameList/src/hu/ak_akademia/main/Main.java
+++ NameList/src/hu/ak_akademia/main/Main.java
@@ -1,18 +1,47 @@
 package hu.ak_akademia.main;
 
-import java.util.Scanner;
-
-import hu.ak_akademia.person.Person;
 import hu.ak_akademia.userinput.UserInput;
+
+//    1.  Lexikografikus sorrendbe    vezetéknév  keresztnév
+//    2.  Lexikografikus sorrendbe    keresztnév  vezetéknév
+//    3.  Fordított lexikografikus sorrendbe  vezetéknév  keresztnév
+//    4.  Fordított lexikografikus sorrendbe  keresztnév  vezetéknév
+//    5.  Nevek teljes hosszúsága alapján növekvő sorrendbe   teljes név hossza   
+//    6.  Nevek teljes hosszúsága alapján csökkenő sorrendbe  teljes név hossza
 
 public class Main {
 
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		Person p = new Person();
-		UserInput ui = new UserInput();
-		ui.getName(sc);
-		sc.close();
-	}
+    public static void main(String[] args) {
+        // UserInput ui = new UserInput();
+        // ui.getName();
+        // System.out.println(ui.getNamesList());
 
+        String strPrice = "15,290";
+        String strMultiplier = "1,15";
+
+        double bsPrice = Double.parseDouble(strPrice.replace(",", "."));
+        double bsMultiplier = Double.parseDouble(strMultiplier.replace(",", "."));
+
+        double multi = bsPrice * bsMultiplier;
+
+        System.out.println(multi);
+
+        System.out.println(round(17.583499999999997, 4));
+        
+        String alma = "17.583499999999997";
+        System.out.println("alma ---->> " + alma);
+        alma = alma.substring(0, alma.length()-1);
+        System.out.println("szeletelt alma ---->> " + alma);
+
+    }
+
+    private static double round(double value, int places) {
+        if (places < 0)
+            throw new IllegalArgumentException();
+
+        long factor = (long) Math.pow(10, places);
+        value = value * factor;
+        long tmp = Math.round(value);
+        return (double) tmp / factor;
+    }
 }
--- NameList/src/hu/ak_akademia/userinput/UserInput.java
+++ NameList/src/hu/ak_akademia/userinput/UserInput.java
@@ -1,30 +1,49 @@
 package hu.ak_akademia.userinput;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Scanner;
-
-import hu.ak_akademia.person.Person;
 
 public class UserInput {
 
-	public void getName(Scanner sc) {
-		System.out.println("A nevek rögzítésekor a vezeték és keresztneveket vesszővel válassza el, amennyiben bevitte az összes nevet, nyomjon egy entert.");
-		String result = "";
-		do {
-			System.out.print("Kérem adja meg a rögzítendő neveket: ");
-			result = sc.nextLine();
-			if(validator(result)) {
-				Person p = new Person();
-				String[] names = result.split(", ");
-				p.setFirstName(names[0]);
-				p.setFirstName(names[1]);
-			}else {
-				System.out.println("A megadott adat érvénytelen.");
-			}
-		}while(result.length() > 0);
-	}
-	
-	private boolean validator(String name) {
-		return name.matches("^([\\D]+([,]{1})[\\D]+)$");
-	}
-	
+    private List<List<String>> namesList = new ArrayList<>();
+
+    public UserInput() {
+    }
+
+    public List<List<String>> getNamesList() {
+        return namesList;
+    }
+
+    public void getName() {
+        Scanner sc = new Scanner(System.in);
+        System.out.println(
+                "A nevek rögzítésekor a vezeték és keresztneveket vesszővel válassza el (Például: Gipsz, Jakab). \nAmennyiben bevitte az összes nevet, nyomjon egy entert.\n");
+        String result = "-";
+        while (result.length() > 0) {
+            System.out.print("Kérem adja meg a rögzítendő neveket: ");
+            result = sc.nextLine();
+            if (result.length() > 0) {
+                namesList.add(fillList(result));
+            }
+        }
+        sc.close();
+    }
+
+    private List<String> fillList(String result) {
+        String[] names = result.split(", ");
+        List<String> tempName = new ArrayList<>();
+        List<String> characteredSortedName = new ArrayList<>();
+        for (String name : names) {
+            char[] letters = new char[name.length()];
+            tempName.add(name);
+            for(int i = 0; i < name.length(); i++) {
+                letters[i] = name.charAt(i);
+            }
+            Arrays.sort(letters);
+            
+        }
+        return tempName;
+    }
 }
