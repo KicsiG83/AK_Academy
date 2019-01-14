98ace9e014fba53e43f4b52e1fbec56e087f33f3
Balogh Gergely <gergely.balogh@icellmobilsoft.hu>
2019-01-14 17:11:54 +0100

    Kisebb változtatás

--- MultiThreadPrimeCalculator/src/hu/ak_akademia/calculator/Calculator.java
+++ MultiThreadPrimeCalculator/src/hu/ak_akademia/calculator/Calculator.java
@@ -3,45 +3,47 @@ package hu.ak_akademia.calculator;
 import java.util.HashSet;
 import java.util.Set;
 
+import hu.ak_akademia.main.Main;
+
 public class Calculator implements Runnable {
 
-	private int min;
-	private int max;
-	private int thread;
-	private Set<Integer> primeNumbers = new HashSet<>();
+    private int min;
+    private int max;
+    private int thread;
+    private Set<Integer> primeNumbers = new HashSet<>();
 
-	public Calculator(int min, int max, int thread) {
-		this.min = min;
-		this.max = max;
-		this.thread = thread;
-	}
+    public Calculator(int min, int max, int thread) {
+        this.min = min;
+        this.max = max;
+        this.thread = thread;
+    }
 
-	@Override
-	public void run() {
-		long startTime = System.nanoTime();
-		for (int i = min; i < max; i++) {
-			if (isPrime(i) == true) {
-				primeNumbers.add(i);
-			}
-			;
-		}
-		System.out.println(
-				"Számolási idő " + thread + " szálon: " + (System.nanoTime() - startTime) / 1000 + " millisecund");
-	}
+    @Override
+    public void run() {
+        long startTime = System.nanoTime();
+        for (int i = min; i < max; i++) {
+            if (isPrime(i) == true) {
+                primeNumbers.add(i);
+            }
+        }
+        Main.setFullSet(primeNumbers);
+        System.out.println("Számolási idő " + thread + " szálon: " + (System.nanoTime() - startTime) / 1000 + " millisecond");
+    }
 
-	private boolean isPrime(int number) {
-		if (number < 2) {
-			return false;
-		}
-		for (int possibleDisvisor = 2; possibleDisvisor < Math.sqrt(number); possibleDisvisor++) {
-			if (number % possibleDisvisor == 0) {
-				return false;
-			}
-		}
-		return true;
-	}
+    private boolean isPrime(int number) {
+        if (number < 2) {
+            return false;
+        }
+        for (int possibleDisvisor = 2; possibleDisvisor < Math.sqrt(number); possibleDisvisor++) {
+            if (number % possibleDisvisor == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
 
-	public Set<Integer> getPrimeNumbers() {
-		return primeNumbers;
-	}
+    public Set<Integer> getPrimeNumbers() {
+        return primeNumbers;
+    }
+
 }
--- MultiThreadPrimeCalculator/src/hu/ak_akademia/main/Main.java
+++ MultiThreadPrimeCalculator/src/hu/ak_akademia/main/Main.java
@@ -1,46 +1,56 @@
 package hu.ak_akademia.main;
 
+import java.util.HashSet;
+import java.util.Set;
+
 import hu.ak_akademia.calculator.Calculator;
 
 public class Main {
 
-	public static void main(String[] args) {
-		Main m = new Main();
-		m.runOneThread();
-		m.runMultiThread();
-		m.runParametersThread(6);
-	}
+    private static Set<Integer> fullSet = new HashSet<>();
 
-	private void runOneThread() {
-		Calculator calc = new Calculator(1, 2_000_000, 1);
-		Thread t1 = new Thread(calc);
-		t1.start();
+    public static void main(String[] args) {
+        Main m = new Main();
+        m.runOneThread();
+        m.runMultiThread();
+        m.runParameterizedThread(6);
+    }
 
-	}
+    private void runOneThread() {
+        Thread t1 = new Thread(new Calculator(1, 2_000_000, 1));
+        t1.start();
 
-	private void runMultiThread() {
-		Calculator c1 = new Calculator(1, 1_000_000, 2);
-		Calculator c2 = new Calculator(1_000_001, 2_000_000, 2);
-		Thread t1 = new Thread(c1);
-		Thread t2 = new Thread(c2);
-		t1.start();
-		t2.start();
-	}
+    }
 
-	private void runParametersThread(int numberOfThreads) {
-		int numberOfMax = 2_000_000;
-		int numberOfResult = numberOfMax / numberOfThreads;
-		int start = 0;
-		int end = numberOfResult;
-		for (int i = 0; i < numberOfThreads; i++) {
-			Calculator c1 = new Calculator(start, end, numberOfThreads);
-			Thread t1 = new Thread(c1);
-			t1.start();
-			start = end + 1;
-			end = start + numberOfResult;
-			if (end > numberOfMax) {
-				end = numberOfMax;
-			}
-		}
-	}
+    private void runMultiThread() {
+        Thread t1 = new Thread(new Calculator(1, 1_000_000, 2));
+        Thread t2 = new Thread(new Calculator(1_000_001, 2_000_000, 2));
+        t1.start();
+        t2.start();
+    }
+
+    private void runParameterizedThread(int numberOfThreads) {
+        int numberOfMax = 2_000_000;
+        int numberOfResult = numberOfMax / numberOfThreads;
+        int start = 0;
+        int end = numberOfResult;
+        for (int i = 0; i < numberOfThreads; i++) {
+            Thread thread = new Thread(new Calculator(start, end, numberOfThreads));
+            thread.start();
+            start = end + 1;
+            end = start + numberOfResult;
+            if (end > numberOfMax) {
+                end = numberOfMax;
+            }
+        }
+    }
+
+    public static void setFullSet(Set<Integer> set) {
+        fullSet.addAll(set);
+    }
+
+    public static Set<Integer> getFullSet() {
+        return fullSet;
+    }
+
 }
