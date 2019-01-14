9e87526b95711e689f9e619e746cc68e5202191a
KicsiG <balogh.gergely83@gmail.com>
2019-01-14 19:29:39 +0100

    MultiThreadPrimeCalculator befejezése + StreamApi

--- MultiThreadPrimeCalculator/src/hu/ak_akademia/calculator/Calculator.java
+++ MultiThreadPrimeCalculator/src/hu/ak_akademia/calculator/Calculator.java
@@ -7,43 +7,47 @@ import hu.ak_akademia.main.Main;
 
 public class Calculator implements Runnable {
 
-    private int min;
-    private int max;
-    private int thread;
-    private Set<Integer> primeNumbers = new HashSet<>();
+	private int min;
+	private int max;
+	private int thread;
+	private Set<Integer> primeNumbers = new HashSet<>();
 
-    public Calculator(int min, int max, int thread) {
-        this.min = min;
-        this.max = max;
-        this.thread = thread;
-    }
+	public Calculator(int min, int max, int thread) {
+		this.min = min;
+		this.max = max;
+		this.thread = thread;
+	}
 
-    @Override
-    public void run() {
-        long startTime = System.nanoTime();
-        for (int i = min; i < max; i++) {
-            if (isPrime(i) == true) {
-                primeNumbers.add(i);
-            }
-        }
-        Main.setFullSet(primeNumbers);
-        System.out.println("Számolási idő " + thread + " szálon: " + (System.nanoTime() - startTime) / 1000 + " millisecond");
-    }
+	@Override
+	public void run() {
+		long startTime = System.nanoTime();
+		for (int i = min; i < max; i++) {
+			if (isPrime(i) == true) {
+				primeNumbers.add(i);
+			}
+		}
+		Main.setFullSet(primeNumbers);
+		System.out.println("Számolási idő " + thread + " szálon: " + min + " - " + max + " - "
+				+ (System.nanoTime() - startTime) / 1_000_000 + " millisecond");
+	}
 
-    private boolean isPrime(int number) {
-        if (number < 2) {
-            return false;
-        }
-        for (int possibleDisvisor = 2; possibleDisvisor < Math.sqrt(number); possibleDisvisor++) {
-            if (number % possibleDisvisor == 0) {
-                return false;
-            }
-        }
-        return true;
-    }
+	public static boolean isPrime(int number) {
+		if (number < 2) {
+			return false;
+		}
+		if (number == 2) {
+			return true;
+		}
+		for (int possibleDisvisor = 2; possibleDisvisor < Math.sqrt(number) + 1; possibleDisvisor++) {
+			if (number % possibleDisvisor == 0) {
+				return false;
+			}
+		}
+		return true;
+	}
 
-    public Set<Integer> getPrimeNumbers() {
-        return primeNumbers;
-    }
+	public Set<Integer> getPrimeNumbers() {
+		return primeNumbers;
+	}
 
 }
--- MultiThreadPrimeCalculator/src/hu/ak_akademia/main/Main.java
+++ MultiThreadPrimeCalculator/src/hu/ak_akademia/main/Main.java
@@ -1,56 +1,96 @@
 package hu.ak_akademia.main;
 
+import java.util.ArrayList;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 import hu.ak_akademia.calculator.Calculator;
 
 public class Main {
 
-    private static Set<Integer> fullSet = new HashSet<>();
+	private static Set<Integer> fullSet = new HashSet<>();
+	private final int MAXNUMBER = 2_000_000;
 
-    public static void main(String[] args) {
-        Main m = new Main();
-        m.runOneThread();
-        m.runMultiThread();
-        m.runParameterizedThread(6);
-    }
+	public static void main(String[] args) {
+		Main m = new Main();
+		m.runOneThread();
+		m.runMultiThread();
+		m.runParameterizedThread(3);
+		m.runStreamApi();
+	}
 
-    private void runOneThread() {
-        Thread t1 = new Thread(new Calculator(1, 2_000_000, 1));
-        t1.start();
+	private void runOneThread() {
+		long startTime = System.nanoTime();
+		Thread t1 = new Thread(new Calculator(1, MAXNUMBER, 1));
+		t1.start();
+		try {
+			t1.join();
+		} catch (Exception e) {
+		}
+		System.out.println("Számolási idő 1 szálon: " + (System.nanoTime() - startTime) / 1_000_000 + " millisecond");
+	}
 
-    }
+	private void runMultiThread() {
+		long startTime = System.nanoTime();
+		Thread t1 = new Thread(new Calculator(1, MAXNUMBER / 2, 2));
+		Thread t2 = new Thread(new Calculator(MAXNUMBER / 2 + 1, MAXNUMBER, 2));
+		t1.start();
+		t2.start();
+		try {
+			t1.join();
+			t2.join();
+		} catch (Exception e) {
+		}
+		System.out.println("Számolási idő 2 szálon: " + (System.nanoTime() - startTime) / 1_000_000 + " millisecond");
+	}
 
-    private void runMultiThread() {
-        Thread t1 = new Thread(new Calculator(1, 1_000_000, 2));
-        Thread t2 = new Thread(new Calculator(1_000_001, 2_000_000, 2));
-        t1.start();
-        t2.start();
-    }
+	private void runParameterizedThread(int numberOfThreads) {
+		List<Thread> listOfThread = new ArrayList<>();
+		long startTime = System.nanoTime();
+		int numberOfMax = MAXNUMBER;
+		int numberOfResult = numberOfMax / numberOfThreads;
+		int start = 0;
+		int end = numberOfResult;
+		for (int i = 0; i < numberOfThreads; i++) {
+			Thread thread = new Thread(new Calculator(start, end, numberOfThreads));
+			thread.start();
+			listOfThread.add(thread);
+			start = end + 1;
+			end = start + numberOfResult;
+			if (end > numberOfMax) {
+				end = numberOfMax;
+			}
+		}
+		for (Thread thread : listOfThread) {
+			try {
+				thread.join();
+			} catch (Exception e) {
+			}
+		}
+		System.out.println("Számolási idő " + numberOfThreads + " szálon: "
+				+ (System.nanoTime() - startTime) / 1_000_000 + " millisecond");
+	}
 
-    private void runParameterizedThread(int numberOfThreads) {
-        int numberOfMax = 2_000_000;
-        int numberOfResult = numberOfMax / numberOfThreads;
-        int start = 0;
-        int end = numberOfResult;
-        for (int i = 0; i < numberOfThreads; i++) {
-            Thread thread = new Thread(new Calculator(start, end, numberOfThreads));
-            thread.start();
-            start = end + 1;
-            end = start + numberOfResult;
-            if (end > numberOfMax) {
-                end = numberOfMax;
-            }
-        }
-    }
+	public synchronized static void setFullSet(Set<Integer> set) {
+		fullSet.addAll(set);
+	}
 
-    public static void setFullSet(Set<Integer> set) {
-        fullSet.addAll(set);
-    }
+	public static Set<Integer> getFullSet() {
+		return fullSet;
+	}
 
-    public static Set<Integer> getFullSet() {
-        return fullSet;
-    }
+	private void runStreamApi() {
+		long startTime = System.nanoTime();
+		test();
+		System.out.println(
+				"Számolási idő StreamApi-val: " + (System.nanoTime() - startTime) / 1_000_000 + " millisecond");
+	}
 
+	private List<Integer> test() {
+		return IntStream.range(1, MAXNUMBER).parallel().filter(number -> Calculator.isPrime(number)).boxed()
+				.collect(Collectors.toList());
+	}
 }
