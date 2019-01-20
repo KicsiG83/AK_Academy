45a4db10ba3cd47b853c1ae3b87681bcd5450525
KicsiG <balogh.gergely83@gmail.com>
2019-01-20 14:16:04 +0100

    Prímszám program módosítása

--- PrimeNumber/src/PrimeNumber.java
+++ PrimeNumber/src/PrimeNumber.java
@@ -20,9 +20,14 @@ public class PrimeNumber {
 		if (number < 2) {
 			return false;
 		}
-		for (int possibleDisvisor = 2; possibleDisvisor < Math.sqrt(number); possibleDisvisor++) {
-			if (number % possibleDisvisor == 0) {
-				return false;
+		if (number == 2) {
+			return true;
+		}
+		if (number % 2 != 0) {
+			for (int possibleDisvisor = 2; possibleDisvisor <= Math.sqrt(number) + 1; possibleDisvisor++) {
+				if (number % possibleDisvisor == 0) {
+					return false;
+				}
 			}
 		}
 		return true;
